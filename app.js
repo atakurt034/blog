781251ef9237f5183ffdec5cec2e7b467f7deef0
atakurt034 <69969985+atakurt034@users.noreply.github.com>
2020-10-27 04:28:51 +0800

    asdf

--- app.js
+++ app.js
@@ -1,44 +1,97 @@
 //jshint esversion:6
-
 const express = require("express");
 const ejs = require("ejs");
 const app = express();
+const fp = require("lodash/fp");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/blog', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+// schema's
+const blogSchema = {
+  title: {
+    type: String,
+    require: [true, "please input title"],
+    min: 1
+  },
+  text: {
+    type: String,
+    require: [true, "Please input text"],
+    min: 1
+  }
+};
+
+// model's
+const Blog = mongoose.model("Blog", blogSchema);
+
+
+// defaults
+const blog1 = new Blog({
+  title: "Home",
+  text: "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing."
+});
+const defaults = [blog1];
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-let posts = [];
-
-app.set("view engine", "ejs");
-
-app.use(express.urlencoded({extended: true}));
-app.use(express.static("public"));
-
-app.get("/", (req, res)=> {
+app.get("/", (req, res) => {
+  Blog.find().then((foundlist)=>{
   res.render("home", {
-    homeContent: homeStartingContent, 
-    poste: posts
+    homeContent: defaults[0].text,
+    poste: foundlist
   });
+  
   });
-
-app.get("/about", (req, res)=> {
-  res.render("about", {aboutContent: aboutContent});
 });
 
-app.get("/contact", (req, res)=> {
-  res.render("contact", {contactContent: contactContent});
+app.get("/about", (req, res) => {
+  res.render("about", {
+    aboutContent: aboutContent
+  });
 });
-app.get("/compose", (req, res)=> {
-  res.render("compose", {contactContent: contactContent});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", {
+    contactContent: contactContent
+  });
 });
-app.post("/compose", (req, res)=> {
+app.get("/compose", (req, res) => {
+  res.render("compose", {
+    contactContent: contactContent
+  });
+});
+app.post("/compose", (req, res)=>{
   const post = {
-  titleInput: req.body.titleInput,
-  bodyInput: req.body.bodyInput,
-  };
-  posts.push(post);
-  res.redirect("/");
+        title: req.body.titleInput,
+        text: req.body.bodyInput,
+      };
+      Blog.insertMany(post).then(()=>{
+        console.log("added " + post);
+      }).catch((err)=>{
+        console.log(err);
+      });
+      res.redirect("/");
+});
+
+app.get("/post/:titlePost", (req, res) => {
+  const titlePost = req.params.titlePost;
+  console.log(titlePost)
+  Blog.findOne({title:titlePost}).then((foundlist)=>{
+      res.render("post", {
+        titlePost:foundlist.title,
+        bodyPost:foundlist.text
+      });
+  });
+
 });
 
 
@@ -47,6 +100,7 @@ app.post("/compose", (req, res)=> {
 
 
 
-app.listen(3000, function() {
+
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
